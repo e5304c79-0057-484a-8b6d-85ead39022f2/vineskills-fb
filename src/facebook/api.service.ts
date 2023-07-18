1a95b10221d706a9469e25ac13f14d696c2a5cf9
HM <hieumdd@gmail.com>
2023-07-19 00:04:55 +0700

    feat: Switch to doppler token

--- src/facebook/api.service.ts
+++ src/facebook/api.service.ts
@@ -1,10 +1,18 @@
 import axios from 'axios';
 
-import { getSecret } from '../secret-manager.service';
 import { API_VERSION } from './facebook.const';
 
 export const getClient = async () => {
-    const accessToken = await getSecret('FB_ACCESS_TOKEN');
+    type DopplerSecretResponse = { value: { raw: string } };
+
+    const accessToken = await axios
+        .request<DopplerSecretResponse>({
+            method: 'GET',
+            url: 'https://api.doppler.com/v3/configs/config/secret',
+            params: { project: 'eaglytics', config: 'prd', name: 'FACEBOOK_ACCESS_TOKEN' },
+            auth: { username: process.env.DOPPLER_TOKEN || '', password: '' },
+        })
+        .then(({ data }) => data.value.raw);
 
     return axios.create({
         baseURL: `https://graph.facebook.com/${API_VERSION}`,
